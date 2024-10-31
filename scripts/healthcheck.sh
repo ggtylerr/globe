a33b56593d91a6540098454fe3b9538f49e6be60
Tyler Flowers <contact@ggtylerr.dev>
2024-10-31 16:18:37 -0400

    Check domains over healthcheck

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 scriptDir=$(dirname "$(readlink -f "$0")")
 
-# TODO: Check if domains are reachable
 for srv in "$scriptDir/../services/"*; do
   srv=${srv%*/}
   cd $srv
@@ -33,5 +32,24 @@ for srv in "$scriptDir/../services/"*; do
       echo "Service $name is still unhealthy."
       curl -H "Priority: urgent" -d "Service $name is unhealthy despite restart!" ntfy.sh/$(<$scriptDir/ntfy.txt)
     fi
+  fi
+  if [ -f "domain.txt" ]; then
+    while IFS= read -r link; do
+      url="https://$link"
+      status_code=$(curl -s -o /dev/null -w "%{http_code}" "$url")
+      if [ "$status_code" -eq 502 ]; then
+        echo "Service $name is 502 over $link"
+        docker compose down && docker compose up -d
+        if $name == "invidious"; then
+          $scriptDir/rotate.sh
+        fi
+        sleep 30
+        status_code=$(curl -s -o /dev/null -w "%{http_code}" "$url")
+        if [ "$status_code" -eq 502 ]; then
+          echo "Service $name is still 502ing"
+          curl -H "Priority: urgent" -d "Service $name cannot be reached over $link!" ntfy.sh/$(<$scriptDir/ntfy.txt)
+        fi
+      fi
+    done < "domain.txt"
   fi
 done
